ccd0b4f58506fb64119f90ec996cf250ef65767a
Wadym <Vadym_Golovko@epam.com>
2023-06-16 02:20:12 +0200

    make cp command async

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -5,9 +5,9 @@ const copy = async () => {
     const path2dir = 'src/fs';
     try{
         await fs.mkdir(path.join(path2dir, 'files_copy'));
-        fs.cp(path.join(path2dir, 'files'),
-                path.join(path2dir, 'files_copy'),
-                { recursive: true });
+        await fs.cp(path.join(path2dir, 'files'),
+                    path.join(path2dir, 'files_copy'),
+                    { recursive: true });
     }catch (err) {
         throw 'FS operation failed'
     }
