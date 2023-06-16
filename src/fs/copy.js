8ffd57c7263be99f0cda93e0fb2f76e18b06d54a
Wadym <Vadym_Golovko@epam.com>
2023-06-16 02:18:15 +0200

    impements copy content of src dir into dest dir

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import fs from 'fs/promises'
+import path from 'path'
+
 const copy = async () => {
-    // Write your code here 
+    const path2dir = 'src/fs';
+    try{
+        await fs.mkdir(path.join(path2dir, 'files_copy'));
+        fs.cp(path.join(path2dir, 'files'),
+                path.join(path2dir, 'files_copy'),
+                { recursive: true });
+    }catch (err) {
+        throw 'FS operation failed'
+    }
 };
 
 await copy();
