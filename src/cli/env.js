52f692e0549f90462cee3ebfa269fba1c1000e34
Wadym <Vadym_Golovko@epam.com>
2023-06-17 18:48:00 -0500

    implements env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,32 @@
+
+// env.js - implement function that parses environment variables with
+// prefix RSS_ and prints them to the console in the format
+// RSS_name1=value1; RSS_name2=value2
+
 const parseEnv = () => {
-    // Write your code here 
+    const envs = process.env;
+    for (let [key, value] of Object.entries(envs)) {
+        if (key.match('^RSS_')) {
+            console.log(`${key}=${value}`)
+        }
+    }
 };
+
+const setEnv = () => {
+    process.env.RSS_1 = 'value1';
+    process.env.RSS_2 = 'value2';
+    process.env.RSS_N = 'valueN';
+}
+const deleteEnv = () => {
+    delete process.env.RSS_1;
+    delete process.env.RSS_2;
+    delete process.env.RSS_N;
+}
+
+setEnv();
+
+parseEnv();
+
+deleteEnv();
 
 parseEnv();
