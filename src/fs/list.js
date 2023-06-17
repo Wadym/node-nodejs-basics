91226a8cebff4fdadf09db8984c9251ad7733243
Wadym <Vadym_Golovko@epam.com>
2023-06-17 05:59:28 -0500

    implements list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,11 +1,21 @@
-import fs from 'fs'
+import fs from 'fs/promises'
 
 // list.js - implement function that prints all array of filenames from files
 // folder into console (if files folder doesn't exists Error with message
 // FS operation failed must be thrown)
 
 const list = async () => {
-    // Write your code here 
+    const path2dir = 'src/fs/files';
+    try{
+        await fs.readdir(path2dir, {recursive: true}).then((files)=>{
+            files.forEach(file => {
+                console.log(file);
+            })}).catch(()=>{
+                throw 'FS operation failed';
+            });
+    }catch (err) {
+        throw 'FS operation failed';
+    }
 };
 
 await list();
