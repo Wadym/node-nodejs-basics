1adde07ba8df003fadbde0ade26ecf5e0e58c698
Wadym <Vadym_Golovko@epam.com>
2023-06-17 21:11:03 -0500

    implements calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+// calcHash.js - implement function that calculates SHA256 hash
+// for file fileToCalculateHashFor.txt and logs it into console as hex
+// requires npm install crypto
+
+import { createHash } from 'node:crypto'
+import fs from 'fs/promises'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const path2file = 'src/hash/files/fileToCalculateHashFor.txt';
+    try {
+        await fs.readFile(path2file,
+            { encoding: 'utf-8' })
+            .then((content)=>{
+                const hex = createHash('sha256')
+                .update(content)
+                .digest('hex')
+                console.log(hex);
+            })
+            .catch((err) => {
+            throw 'FS operation failed';
+        })
+    } catch (err) {
+        throw 'FS operation failed';
+    }
 };
 
 await calculateHash();
