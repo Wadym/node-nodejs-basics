687d6caddab4b73e693826133031fc9ab975553a
Wadym <Vadym_Golovko@epam.com>
2023-06-18 17:36:59 -0500

    implements decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+// decompress.js - implement function that decompresses archive.gz
+// back to the fileToCompress.txt with same content as before compression
+// using zlib and Streams API
+
+import { createGzip, createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { promisify } from 'node:util';
+
 const decompress = async () => {
-    // Write your code here 
+    const path2InFile = 'src/zip/files/archive.gz';
+    const path2OutFile = 'src/zip/files/fileToCompressOut.txt'
+    const pipe = promisify(pipeline);
+    const gunzip = createGunzip();
+    const source = createReadStream(path2InFile);
+    const destination = createWriteStream(path2OutFile);
+    await pipe(source, gunzip, destination);
 };
 
 await decompress();
